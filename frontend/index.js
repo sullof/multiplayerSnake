f88c27f47becf6d81fe04a55cae18049a38e7374
thespacemaker <me@adnan.io>
2021-02-23 14:51:40 -0600

    added background

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@
 const SNAKE_COLOUR = '#749ba8';
 const FOOD_COLOUR = '#e66916';
 
-const socket = io('http://localhost:3000');
+const socket = io('http://3.139.87.87:3000');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
@@ -16,17 +16,18 @@ window.onscroll = function () { window.scrollTo(0, 0); };
 const gameScreen = document.getElementById('gameScreen');
 const initialScreen = document.getElementById('initialScreen');
 const newGameBtn = document.getElementById('newGameButton');
-const canvas = document.getElementById('canvas');
+const gcanvas = document.getElementById('gameCanvas');
 const joinGameBtn = document.getElementById('joinGameButton');
 const gameCodeInput = document.getElementById('gameCodeInput');
 const time = document.getElementById('time');
 // const gameCodeDisplay = document.getElementById('gameCodeDisplay');
 const img = document.getElementById('colorImage');
-const toolbar = document.getElementById('toolbar').clientHeight + 200
-const windowHeight = document.documentElement.clientHeight - toolbar
-const windowWidth = document.documentElement.clientWidth - 200
-canvas.width = Math.floor(windowWidth/40) * 40
-canvas.height = Math.floor(windowHeight/40) * 40
+// const toolbar = document.getElementById('toolbar').clientHeight + 200
+// const windowHeight = document.documentElement.clientHeight - toolbar
+const windowHeight = window.innerHeight - 200
+const windowWidth = window.innerWidth - 200
+gcanvas.width = Math.floor(windowWidth/40) * 40
+gcanvas.height = Math.floor(windowHeight/40) * 40
 
 // window.onscroll = function () { window.scrollTo(0, 0); };
 newGameBtn.addEventListener('click', newGame);
@@ -34,6 +35,7 @@ joinGameBtn.addEventListener('click', joinGame);
 
 
 function newGame() {
+  console.log("newGame")
   socket.emit('newGame');
   // init();
 }
@@ -44,8 +46,8 @@ function joinGame() {
   const message = {
     roomName: code,
     screenSize: {
-      width: canvas.width,
-      height: canvas.height
+      width: gcanvas.width,
+      height: gcanvas.height
     }
   }
   console.log(message)
@@ -59,10 +61,11 @@ function init() {
   initialScreen.style.display = "none";
   gameScreen.style.display = "block";
 
-  // canvas = document.getElementById('canvas');
-  ctx = canvas.getContext('2d');
+  // gcanvas = document.getElementById('gcanvas');
+  ctx3 = gcanvas.getContext('2d');
 
-  
+  // ctx3.fillStyle = BG_COLOUR;
+  // ctx3.fillRect(0, 0, gcanvas.width, gcanvas.height);
 
  
   // ctx.fillStyle = BG_COLOUR;
@@ -80,29 +83,18 @@ function paintGame(state) {
   img.src=state.imgURL;
   // console.log(state.sinceLastFood)
   time.innerText = state.currentTime;
-
-  /*
-
-    gradient background
-
-  */
-  var my_gradient = ctx.createLinearGradient(0, 0, 0, 370);
-  my_gradient.addColorStop(.5, "#28db96");
-  my_gradient.addColorStop(1, "#38e4a2");
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  ctx.fillStyle = my_gradient;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx3.clearRect(0, 0, gcanvas.width, gcanvas.height);
+  // ctx3.fillStyle = BG_COLOUR;
+  // ctx3.fillRect(0, 0, gcanvas.width, gcanvas.height);
   if (state.food){
     let food = state.food
     // const gridsize = state.gridsize;
-    const sizeX = canvas.width /state.gridX
-    const sizeY = canvas.height/state.gridY
-
-    ctx.fillStyle = state.food[0].color.hex;
-    ctx.fillRect(food[0].x * sizeX, food[0].y * sizeY, 40, 40);
-    ctx.fillStyle = state.food[1].color.hex;
-    ctx.fillRect(food[1].x * sizeX, food[1].y * sizeY, 40, 40);
+    const sizeX = gcanvas.width /state.gridX
+    const sizeY = gcanvas.height/state.gridY
+    ctx3.fillStyle = state.food[0].color.hex;
+    ctx3.fillRect(food[0].x * sizeX, food[0].y * sizeY, 40, 40);
+    ctx3.fillStyle = state.food[1].color.hex;
+    ctx3.fillRect(food[1].x * sizeX, food[1].y * sizeY, 40, 40);
 
     paintPlayer(state.players[0], sizeX, sizeY, SNAKE_COLOUR);
     paintPlayer(state.players[1], sizeX, sizeY, 'red');
@@ -112,9 +104,9 @@ function paintGame(state) {
 function paintPlayer(playerState, sizeX, sizeY, colour) {
   const snake = playerState.snake;
 
-  ctx.fillStyle = colour;
+  ctx3.fillStyle = colour;
   for (let cell of snake) {
-    ctx.fillRect(cell.x * sizeX, cell.y * sizeY, 40, 40);
+    ctx3.fillRect(cell.x * sizeX, cell.y * sizeY, 40, 40);
   }
 }
 
