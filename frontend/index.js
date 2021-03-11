624a640456f35b9b5c32a1e5e94307ecd26430d9
thespacemaker <me@adnan.io>
2021-03-11 16:03:51 -0600

    updated endpoints

--- frontend/index.js
+++ frontend/index.js
@@ -2,12 +2,12 @@ const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#ffffff';
 const FOOD_COLOUR = '#e66916';
 
-// const socket = io('http://3.139.87.87:3000');
-// const socketPractice = io('http://3.133.132.75:3000');
+const socket = io('http://3.139.87.87:3000');
+const socketPractice = io('http://3.133.132.75:3000');
 
 //For Development
-const socket = io('http://192.168.1.12:3000');
-const socketPractice = io('http://192.168.1.12:3000');
+// const socket = io('http://192.168.1.12:3000');
+// const socketPractice = io('http://192.168.1.12:3000');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
