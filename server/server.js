416d55bea6d4491ecd925e3a0fb6e337f01d362b
Eryk-Luiz <erykluizvieira@gmail.com>
2021-04-08 22:35:34 -0300

    Minor adjuts

--- server/server.js
+++ server/server.js
@@ -74,129 +74,159 @@ io.on('connection', client => {
     }
   
     async function handleRecievedCaptcha(data) {
-      let roomName = clientRooms[client.id]
-      stopSendingCaptcha(roomName)
+      newRelic.startBackgroundTransaction('recievedCaptcha', () => {
+        let roomName = clientRooms[client.id]
+        stopSendingCaptcha(roomName)
+
+        newRelic.endTransaction();
+      })
     }
   
     async function handleConfirmedScore(data) {
-      let roomName = data
-      state[data].confirmed = true
+      newRelic.startBackgroundTransaction('confirmedScore', () => {
+        let roomName = data
+        state[data].confirmed = true
+
+        newRelic.endTransaction();
+      })
     }
   
     async function handleNewGame() {
-      let roomName = makeid(10);
-      clientRooms[client.id] = roomName;
-      client.emit('gameCode', roomName);
-      console.log(roomName)
-  
-      client.join(roomName);
-      client.number = 2;
-      client.emit('init', 2);
+      newRelic.startBackgroundTransaction('newGame', () => {
+        let roomName = makeid(10);
+        clientRooms[client.id] = roomName;
+        client.emit('gameCode', roomName);
+        console.log(roomName)
+    
+        client.join(roomName);
+        client.number = 2;
+        client.emit('init', 2);
+
+        newRelic.endTransaction();
+      })
     }
   
     async function handleKeydown(keyCode) {
-      // console.log('keystroke', client.id)
-      const roomName = clientRooms[client.id];
-      if (!roomName) {
-        return;
-      }
-      try {
-        keyCode = parseInt(keyCode);
-      } catch(e) {
-        console.error(e);
-        return;
-      }
-      try {
-        if(state[roomName]) {
-          currentVelocity = state[roomName].players[0].vel
-          const vel = getUpdatedVelocity(keyCode, currentVelocity, state[roomName]);
-  
-          if (vel) {
-            try {
-              state[roomName].players[client.number - 1].vel = vel;
-            }
-            catch(error) {
-              console.log('caught some shit trying to set velocity!')
-              console.log(error)
+      newRelic.startBackgroundTransaction('Keydown',() => {
+          // console.log('keystroke', client.id)
+        const roomName = clientRooms[client.id];
+        if (!roomName) {
+          return;
+        }
+        try {
+          keyCode = parseInt(keyCode);
+        } catch(e) {
+          console.error(e);
+          return;
+        }
+        try {
+          if(state[roomName]) {
+            currentVelocity = state[roomName].players[0].vel
+            const vel = getUpdatedVelocity(keyCode, currentVelocity, state[roomName]);
+    
+            if (vel) {
+              try {
+                state[roomName].players[client.number - 1].vel = vel;
+              }
+              catch(error) {
+                console.log('caught some shit trying to set velocity!')
+                console.log(error)
+              }
             }
           }
         }
-      }
-      catch(error) {
-        console.log('caught some shit getting velocity!')
-        console.log(error)
-      }
-  
+        catch(error) {
+          console.log('caught some shit getting velocity!')
+          console.log(error)
+        }
+
+
+        newRelic.endTransaction();
+      })
     }
 });
 
 
 function startGameInterval(roomName) {
-  const intervalId = setInterval(() => {
+  newRelic.startBackgroundTransaction('startGameInterval', () => {
+    const intervalId = setInterval(() => {
 
-    const winner = gameLoop(state[roomName]);
-    const url = sendCaptcha(roomName)
-
-    if (!winner) {
-      try {
-        emitGameState(roomName, state[roomName])
-        if (url) {
-          console.log('theres a captcha')
-          emitCaptcha(roomName, url)
+      const winner = gameLoop(state[roomName]);
+      const url = sendCaptcha(roomName)
+  
+      if (!winner) {
+        try {
+          emitGameState(roomName, state[roomName])
+          if (url) {
+            console.log('theres a captcha')
+            emitCaptcha(roomName, url)
+          }
         }
+        catch(error) {
+          console.log(error)
+        }
+      } else {
+        emitGameOver(roomName, winner);
+        state[roomName].endTime = new Date()
+        state[roomName] = null;
+        clearInterval(intervalId);
       }
-      catch(error) {
-        console.log(error)
-      }
-    } else {
-      emitGameOver(roomName, winner);
-      state[roomName].endTime = new Date()
-      state[roomName] = null;
-      clearInterval(intervalId);
-    }
-  }, 1000 / FRAME_RATE);
+    }, 1000 / FRAME_RATE);
+
+    newRelic.endTransaction();
+  })
 }
 
 function emitGameState(room, gameState) {
-  // Send this event to everyone in the room.
-  const player = gameState.players[0]
-  const food = gameState.food
-  const grid = {
-    x: gameState.gridX,
-    y: gameState.gridY
-  }
-  const lengthOfSnake = player.snake.length
-  var bufArr = new ArrayBuffer(11+(lengthOfSnake * 2))
-  var bufView = new Uint8Array(bufArr)
-  bufView[0]=player.pos.x
-  bufView[1]=player.pos.y
-  bufView[2]=food[0].x
-  bufView[3]=food[0].y
-  bufView[4]=food[1].x
-  bufView[5]=food[1].y
-  bufView[6]= food[0].index
-  bufView[7]= food[1].index
-  bufView[8]=grid.x
-  bufView[9]=grid.y
-  bufView[10]=gameState.currentTime
-  for (let x = 0; x < (lengthOfSnake); x++) {
-    let index = 11 + (x * 2)
-    bufView[index]=player.snake[x].x
-    bufView[index+1]=player.snake[x].y
-  }
-  // console.log(bufView)
-  // console.log(bufView)
-  // io.sockets.in(room)
-  //   // .emit('gameState', JSON.stringify(gameState))
-  //   .emit('gameState', bufArr)
-  io.to(gameState.clientID).emit('gameState', bufArr);
+  newRelic.startBackgroundTransaction('emitGameState', () => {
+    // Send this event to everyone in the room.
+    const player = gameState.players[0]
+    const food = gameState.food
+    const grid = {
+      x: gameState.gridX,
+      y: gameState.gridY
+    }
+    const lengthOfSnake = player.snake.length
+    var bufArr = new ArrayBuffer(11+(lengthOfSnake * 2))
+    var bufView = new Uint8Array(bufArr)
+    bufView[0]=player.pos.x
+    bufView[1]=player.pos.y
+    bufView[2]=food[0].x
+    bufView[3]=food[0].y
+    bufView[4]=food[1].x
+    bufView[5]=food[1].y
+    bufView[6]= food[0].index
+    bufView[7]= food[1].index
+    bufView[8]=grid.x
+    bufView[9]=grid.y
+    bufView[10]=gameState.currentTime
+    for (let x = 0; x < (lengthOfSnake); x++) {
+      let index = 11 + (x * 2)
+      bufView[index]=player.snake[x].x
+      bufView[index+1]=player.snake[x].y
+    }
+    // console.log(bufView)
+    // console.log(bufView)
+    // io.sockets.in(room)
+    //   // .emit('gameState', JSON.stringify(gameState))
+    //   .emit('gameState', bufArr)
+    io.to(gameState.clientID).emit('gameState', bufArr);
+
+    newRelic.endTransaction();
+  })
+
+  
 }
 
 function emitCaptcha(room, url) {
-  // Send this event to everyone in the room.
-  console.log('emitting captcha')
-  io.sockets.in(room)
-    .emit('captcha', url);
+  newRelic.startBackgroundTransaction('emitCaptcha', () => {
+     // Send this event to everyone in the room.
+    console.log('emitting captcha')
+    io.sockets.in(room).emit('captcha', url);
+
+    newRelic.endTransaction();
+  })
+ 
 }
 
 function emitGameOver(room, winner) {
