ee7158e0b7a6b98fc6e3e571a18dcc119e5525c6
Eryk-Luiz <erykluizvieira@gmail.com>
2021-04-11 17:03:43 -0300

    Fixed cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const io = require('socket.io')(null,{
-  origins: 'snakedev.frontend.scoremilk.com'
+  origins: 'https://snakedev.frontend.scoremilk.com'
 });
 const newRelic = require('newrelic');
 
