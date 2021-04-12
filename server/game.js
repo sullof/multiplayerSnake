03cb6448afe5e80581dad3ae1a081d3093b5d0c6
Eryk-Luiz <erykluizvieira@gmail.com>
2021-04-12 20:53:05 -0300

    Intrumentation to game.js

--- server/game.js
+++ server/game.js
@@ -1,3 +1,4 @@
+const newRelic = require('newrelic');
 const { IMAGES, COLORS, GRID_SIZE } = require('./constants');
 let poisonFood = null
 let atePoison = false
@@ -14,35 +15,54 @@ module.exports = {
 }
 
 function initGame(roomName) {
-  const state = createGameState(roomName)
-  randomFood(state);
-  return state;
+  newRelic.startBackgroundTransaction('initGame', () => {
+    const state = createGameState(roomName)
+    randomFood(state);
+
+    newRelic.endTransaction();
+    return state;
+  })
 }
 
 function initPoison(roomName) {
-  poison[roomName] = {
-    number: null,
-    url: null,
-    needToSend: false
-  }
+  newRelic.startBackgroundTransaction('initPoison', () => {
+    poison[roomName] = {
+      number: null,
+      url: null,
+      needToSend: false
+    }
+
+    newRelic.endTransaction();
+  })
 }
 
 function sendCaptcha(roomName) {
-  if (poison[roomName].needToSend){
-    return poison[roomName].url
-  }
-  else {
-    return null
-  }
+  newRelic.startBackgroundTransaction('sendCaptcha', () => {
+    if (poison[roomName].needToSend){
+      newRelic.endTransaction();
+      return poison[roomName].url
+    }
+    else {
+      newRelic.endTransaction();
+      return null
+    }
+    
+  })
+
+  
 }
 
 function stopSendingCaptcha(roomName) {
-  try {
-    poison[roomName].needToSend = false
-  }
-  catch {
-    console.log('no poison yet')
-  }
+  newRelic.startBackgroundTransaction('stopSendingCaptcha', () => {
+    try {
+      poison[roomName].needToSend = false
+    }
+    catch {
+      console.log('no poison yet')
+    }
+
+    newRelic.endTransaction();
+  })
 }
 
 function createGameState(roomName) {
@@ -96,84 +116,96 @@ function createGameState(roomName) {
 }
 
 function gameLoop(state) {
-  if (!state) {
-    return;
-  }
-  if (state.timer < new Date()) {
-    return 2;
-  }
-  if(state.lastFood) {
-    state.sinceLastFood = new Date().getTime() - state.lastFood.getTime()
-  }
-  if (state.timer) {
-    state.currentTime = state.timer.getTime() - new Date().getTime()
-    state.currentTime = Math.floor(state.currentTime/1000)
-  }
-  const playerOne = state.players[0];
-  const playerTwo = state.players[1];
-  gridWidth = state.gridX
-  gridHeight = state.gridY
-  playerOne.pos.x += playerOne.vel.x;
-  playerOne.pos.y += playerOne.vel.y;
-
-  if (playerOne.pos.x < 0 || playerOne.pos.x > (gridWidth - 1) || playerOne.pos.y < 0 || playerOne.pos.y > (gridHeight - 1)) {
-    console.log('player outside')
-    return 2;
-  }
-
-  if (state.food[0].x === playerOne.pos.x && state.food[0].y === playerOne.pos.y) {
-    if(checkIfPoison(state, 0)) {
-      return 2
+  newRelic.startBackgroundTransaction('gameLoop', () => {
+    if (!state) {
+      return;
     }
-  }
-
-  if (state.food[1].x === playerOne.pos.x && state.food[1].y === playerOne.pos.y) {
-    if(checkIfPoison(state, 1)) {
-      return 2
+    if (state.timer < new Date()) {
+      return 2;
     }
-  }
-
-  if (playerOne.vel.x || playerOne.vel.y) {
-    for (let cell of playerOne.snake) {
-      if (cell.x === playerOne.pos.x && cell.y === playerOne.pos.y) {
-        return 2;
+    if(state.lastFood) {
+      state.sinceLastFood = new Date().getTime() - state.lastFood.getTime()
+    }
+    if (state.timer) {
+      state.currentTime = state.timer.getTime() - new Date().getTime()
+      state.currentTime = Math.floor(state.currentTime/1000)
+    }
+    const playerOne = state.players[0];
+    const playerTwo = state.players[1];
+    gridWidth = state.gridX
+    gridHeight = state.gridY
+    playerOne.pos.x += playerOne.vel.x;
+    playerOne.pos.y += playerOne.vel.y;
+  
+    if (playerOne.pos.x < 0 || playerOne.pos.x > (gridWidth - 1) || playerOne.pos.y < 0 || playerOne.pos.y > (gridHeight - 1)) {
+      console.log('player outside')
+      return 2;
+    }
+  
+    if (state.food[0].x === playerOne.pos.x && state.food[0].y === playerOne.pos.y) {
+      if(checkIfPoison(state, 0)) {
+        return 2
       }
     }
-
-    playerOne.snake.push({ ...playerOne.pos });
-    playerOne.snake.shift();
-  }
-
-  if (playerTwo.vel.x || playerTwo.vel.y) {
-    for (let cell of playerTwo.snake) {
-      if (cell.x === playerTwo.pos.x && cell.y === playerTwo.pos.y) {
-        return 1;
+  
+    if (state.food[1].x === playerOne.pos.x && state.food[1].y === playerOne.pos.y) {
+      if(checkIfPoison(state, 1)) {
+        return 2
       }
     }
+  
+    if (playerOne.vel.x || playerOne.vel.y) {
+      for (let cell of playerOne.snake) {
+        if (cell.x === playerOne.pos.x && cell.y === playerOne.pos.y) {
+          return 2;
+        }
+      }
+  
+      playerOne.snake.push({ ...playerOne.pos });
+      playerOne.snake.shift();
+    }
+  
+    if (playerTwo.vel.x || playerTwo.vel.y) {
+      for (let cell of playerTwo.snake) {
+        if (cell.x === playerTwo.pos.x && cell.y === playerTwo.pos.y) {
+          return 1;
+        }
+      }
+  
+      playerTwo.snake.push({ ...playerTwo.pos });
+      playerTwo.snake.shift();
+    }
+  
+    newRelic.endTransaction();
 
-    playerTwo.snake.push({ ...playerTwo.pos });
-    playerTwo.snake.shift();
-  }
+    return false;
+  })
 
-  return false;
+  
 }
 
 function checkIfPoison(state, foodNumber) {
-  if (foodNumber === poison[state.roomName].number) {
-    return true;
-  } else {
-    const playerOne = state.players[0];
-    playerOne.snake.push({ ...playerOne.pos });
-    playerOne.pos.x += playerOne.vel.x;
-    playerOne.pos.y += playerOne.vel.y;
-    state.lastFood = new Date()
-    state.foodTimes.push(state.lastFood.getTime() - state.startTime.getTime())
-    randomFood(state);
-  }
+  newRelic.startBackgroundTransaction('checkIfPoison', () => {
+    if (foodNumber === poison[state.roomName].number) {
+      newRelic.endTransaction();
+      return true;
+    } else {
+      const playerOne = state.players[0];
+      playerOne.snake.push({ ...playerOne.pos });
+      playerOne.pos.x += playerOne.vel.x;
+      playerOne.pos.y += playerOne.vel.y;
+      state.lastFood = new Date()
+      state.foodTimes.push(state.lastFood.getTime() - state.startTime.getTime())
+      randomFood(state);
+
+      newRelic.endTransaction();
+    }
+  })
 }
 
 function randomFood(state) {
-  gridWidth = state.gridX - 2
+  newRelic.startBackgroundTransaction('randomFood', () => {
+    gridWidth = state.gridX - 2
   gridHeight = state.gridY - 2
   const food = [
     {
@@ -222,168 +254,184 @@ function randomFood(state) {
   state.food = food;
   state.sinceLastFood = 0;
   randomColors(state)
+
+  newRelic.endTransaction();
+  })  
 }
 
 function randomColors(state) {
-  let firstNumber = Math.floor(Math.random() * COLORS.length)
-  let secondNumber = Math.floor(Math.random() * COLORS.length)
-  if (firstNumber == secondNumber) {
-    randomColors(state)
-  } else {
-    state.food[0].color = COLORS[firstNumber]
-    state.food[0].index = firstNumber
-    state.food[1].color = COLORS[secondNumber]
-    state.food[1].index = secondNumber
-    randomPoison(state);
-  }
+  newRelic.startBackgroundTransaction('randomColors', () => {
+    let firstNumber = Math.floor(Math.random() * COLORS.length)
+    let secondNumber = Math.floor(Math.random() * COLORS.length)
+    if (firstNumber == secondNumber) {
+      randomColors(state)
+    } else {
+      state.food[0].color = COLORS[firstNumber]
+      state.food[0].index = firstNumber
+      state.food[1].color = COLORS[secondNumber]
+      state.food[1].index = secondNumber
+      randomPoison(state);
+    }
+
+    newRelic.endTransaction();
+  })
 }
 
 function randomPoison(state) {
-  console.time('Captcha')
-  poison[state.roomName].number = Math.round(Math.random())
-  poisonColor = state.food[poison[state.roomName].number].color.name
-  let array = IMAGES.filter(image => image.color == poisonColor)
-  let rand = Math.floor(Math.random() * array[0].imgURLs.length)
-  poison[state.roomName].url = array[0].imgURLs[rand]
-  poison[state.roomName].needToSend = true
-  state.imgURL = array[0].imgURLs[rand]
-  console.timeEnd('Captcha')
+  newRelic.startBackgroundTransaction('randomPoison', () => {
+    poison[state.roomName].number = Math.round(Math.random())
+    poisonColor = state.food[poison[state.roomName].number].color.name
+    let array = IMAGES.filter(image => image.color == poisonColor)
+    let rand = Math.floor(Math.random() * array[0].imgURLs.length)
+    poison[state.roomName].url = array[0].imgURLs[rand]
+    poison[state.roomName].needToSend = true
+    state.imgURL = array[0].imgURLs[rand]
+ 
+
+    newRelic.endTransaction();
+  })
+
+  
 }
 
 function getUpdatedVelocity(keyCode, currentVelocity, state) {
-  console.log(keyCode)
-  let player = state.players[0]
-  switch (keyCode) {
-    case 37: { // left
-      let move = { x: -1, y: 0 }
-      let newBlock = {}
-      newBlock.x = player.pos.x + move.x
-      newBlock.y = player.pos.y + move.y
-      if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
-        return null
-      } else {
-        if (currentVelocity.x != 1) {
-          return { x: -1, y: 0 };
-        }
-        else {
+  newRelic.startBackgroundTransaction('getUpdatedVelocity', () => {
+    console.log(keyCode)
+    let player = state.players[0]
+    switch (keyCode) {
+      case 37: { // left
+        let move = { x: -1, y: 0 }
+        let newBlock = {}
+        newBlock.x = player.pos.x + move.x
+        newBlock.y = player.pos.y + move.y
+        if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
           return null
+        } else {
+          if (currentVelocity.x != 1) {
+            return { x: -1, y: 0 };
+          }
+          else {
+            return null
+          }
+        }
+      }
+      case 38: { // down
+        let move = { x: 0, y: -1 }
+        let newBlock = {}
+        newBlock.x = player.pos.x + move.x
+        newBlock.y = player.pos.y + move.y
+        if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
+          return null
+        } else {
+          if (currentVelocity.y != 1) {
+            return { x: 0, y: -1 };
+          }
+          else {
+            return null
+          }
+        }
+      }
+      case 39: { // right
+        let move = { x: 1, y: 0 }
+        let newBlock = {}
+        newBlock.x = player.pos.x + move.x
+        newBlock.y = player.pos.y + move.y
+        if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
+          return null
+        } else {
+          if (currentVelocity.x != -1) {
+            return { x: 1, y: 0 };
+          }
+          else {
+            return null
+          }
+        }
+      }
+      case 40: { // up
+        let move = { x: 0, y: 1 }
+        let newBlock = {}
+        newBlock.x = player.pos.x + move.x
+        newBlock.y = player.pos.y + move.y
+        if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
+          return null
+        } else {
+          if (currentVelocity.y != -1) {
+            return { x: 0, y: 1 };
+          }
+          else {
+            return null
+          }
+        }
+      }
+      case 65: { // left
+        let move = { x: -1, y: 0 }
+        let newBlock = {}
+        newBlock.x = player.pos.x + move.x
+        newBlock.y = player.pos.y + move.y
+        if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
+          // console.log('moving backwards')
+          return null
+        } else {
+          if (currentVelocity.x != 1) {
+            // console.log('inside')
+            return { x: -1, y: 0 };
+          }
+          else {
+            // console.log('nothing')
+            return null
+          }
+        }
+      }
+      case 87: { // down
+        let move = { x: 0, y: -1 }
+        let newBlock = {}
+        newBlock.x = player.pos.x + move.x
+        newBlock.y = player.pos.y + move.y
+        if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
+          return null
+        } else {
+          if (currentVelocity.y != 1) {
+            return { x: 0, y: -1 };
+          }
+          else {
+            return null
+          }
+        }
+      }
+      case 68: { // right
+        let move = { x: 1, y: 0 }
+        let newBlock = {}
+        newBlock.x = player.pos.x + move.x
+        newBlock.y = player.pos.y + move.y
+        if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
+          return null
+        } else {
+          if (currentVelocity.x != -1) {
+            return { x: 1, y: 0 };
+          }
+          else {
+            return null
+          }
+        }
+      }
+      case 83: { // up
+        let move = { x: 0, y: 1 }
+        let newBlock = {}
+        newBlock.x = player.pos.x + move.x
+        newBlock.y = player.pos.y + move.y
+        if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
+          return null
+        } else {
+          if (currentVelocity.y != -1) {
+            return { x: 0, y: 1 };
+          }
+          else {
+            return null
+          }
         }
       }
     }
-    case 38: { // down
-      let move = { x: 0, y: -1 }
-      let newBlock = {}
-      newBlock.x = player.pos.x + move.x
-      newBlock.y = player.pos.y + move.y
-      if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
-        return null
-      } else {
-        if (currentVelocity.y != 1) {
-          return { x: 0, y: -1 };
-        }
-        else {
-          return null
-        }
-      }
-    }
-    case 39: { // right
-      let move = { x: 1, y: 0 }
-      let newBlock = {}
-      newBlock.x = player.pos.x + move.x
-      newBlock.y = player.pos.y + move.y
-      if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
-        return null
-      } else {
-        if (currentVelocity.x != -1) {
-          return { x: 1, y: 0 };
-        }
-        else {
-          return null
-        }
-      }
-    }
-    case 40: { // up
-      let move = { x: 0, y: 1 }
-      let newBlock = {}
-      newBlock.x = player.pos.x + move.x
-      newBlock.y = player.pos.y + move.y
-      if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
-        return null
-      } else {
-        if (currentVelocity.y != -1) {
-          return { x: 0, y: 1 };
-        }
-        else {
-          return null
-        }
-      }
-    }
-    case 65: { // left
-      let move = { x: -1, y: 0 }
-      let newBlock = {}
-      newBlock.x = player.pos.x + move.x
-      newBlock.y = player.pos.y + move.y
-      if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
-        // console.log('moving backwards')
-        return null
-      } else {
-        if (currentVelocity.x != 1) {
-          // console.log('inside')
-          return { x: -1, y: 0 };
-        }
-        else {
-          // console.log('nothing')
-          return null
-        }
-      }
-    }
-    case 87: { // down
-      let move = { x: 0, y: -1 }
-      let newBlock = {}
-      newBlock.x = player.pos.x + move.x
-      newBlock.y = player.pos.y + move.y
-      if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
-        return null
-      } else {
-        if (currentVelocity.y != 1) {
-          return { x: 0, y: -1 };
-        }
-        else {
-          return null
-        }
-      }
-    }
-    case 68: { // right
-      let move = { x: 1, y: 0 }
-      let newBlock = {}
-      newBlock.x = player.pos.x + move.x
-      newBlock.y = player.pos.y + move.y
-      if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
-        return null
-      } else {
-        if (currentVelocity.x != -1) {
-          return { x: 1, y: 0 };
-        }
-        else {
-          return null
-        }
-      }
-    }
-    case 83: { // up
-      let move = { x: 0, y: 1 }
-      let newBlock = {}
-      newBlock.x = player.pos.x + move.x
-      newBlock.y = player.pos.y + move.y
-      if (newBlock.x === player.snake[player.snake.length - 2].x && newBlock.y === player.snake[player.snake.length - 2].y) {
-        return null
-      } else {
-        if (currentVelocity.y != -1) {
-          return { x: 0, y: 1 };
-        }
-        else {
-          return null
-        }
-      }
-    }
-  }
+
+    newRelic.endTransaction();
+  })
 }
